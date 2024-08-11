16b9dc062357966550fb388793d420e6ff233cd9
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2024-08-11 13:31:13 -0300

    Cors configuration

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,17 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const cors = {
+    origin: ['http://localhost:5173', 'http://localhost'],
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+  };
+
+  app.enableCors(cors);
+
   app.use(helmet());
 
   const config = new DocumentBuilder()
