9bad5d51e7f40a0b4b523ea72851a35424ae453e
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2024-11-11 21:09:20 -0300

    Frontend domain release in cors policy

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
       'http://localhost',
       'https://www.erp.meviosshop.com.br',
       'https://erp.meviosshop.com.br/',
+      'https://mini-erp-react-app.vercel.app/',
     ],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
     preflightContinue: false,
